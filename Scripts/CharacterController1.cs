564f6a021ce7d919de3e66b60a2be6d7459003a2
bora-c <boraboracebi@gmail.com>
2020-10-08 22:41:41 +0300

    crouch eklenecek çok iş var :3

--- Scripts/CharacterController1.cs
+++ Scripts/CharacterController1.cs
@@ -65,7 +65,7 @@ public class CharacterController1 : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftControl)) 
         {
             playerCamera.transform.position = player.transform.position + crouchOffset;
-            Debug.Log("hay amk basma"); 
+            Debug.Log("basmasana ctrl'ye :3"); 
         }
 
             // rotasyon
